633ec66335f47f14dfc5ac1a2ebc64230ea4dbde
S4TU9N <148907943+S4TU9N@users.noreply.github.com>
2026-05-27 12:03:59 -0400

    fix variable name mismatch

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const SUPABASE_URL =
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImticGNnZHNmcW9zZ2VvYWFuZ2hmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NDQ0NDEsImV4cCI6MjA5NDMyMDQ0MX0.rAZYev_j43ADqXw3jnXakxZFH0MwTP5S9-t3vbzhujg";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
